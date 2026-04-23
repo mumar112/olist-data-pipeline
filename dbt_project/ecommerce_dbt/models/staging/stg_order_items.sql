e7e1611b6fff77b4627fe396ab3f2b1595426c29
Mohammed Umar <90332434+mumar112@users.noreply.github.com>
2026-04-23 02:28:46 -0400

    Fix text addition cast in stg_order_items

--- dbt_project/ecommerce_dbt/models/staging/stg_order_items.sql
+++ dbt_project/ecommerce_dbt/models/staging/stg_order_items.sql
@@ -11,7 +11,7 @@ cleaned as (
         shipping_limit_date::timestamp as shipping_limit_at,
         price::numeric(10,2)     as price,
         freight_value::numeric(10,2) as freight_value,
-        (price + freight_value)::numeric(10,2) as total_item_value
+        (price::numeric(10,2) + freight_value::numeric(10,2)) as total_item_value
     from source
     where order_id is not null
       and price is not null
